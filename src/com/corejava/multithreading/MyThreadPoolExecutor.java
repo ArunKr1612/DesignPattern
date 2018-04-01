f5823278e9f4ec6923120f8830dcda4142a1d339
ArunKr1612 <silicon87arun@gmail.com>
2018-04-01 17:18:35 +0530

    Corejava Thread-pool-executor Concepts

---
+++ src/com/corejava/multithreading/MyThreadPoolExecutor.java
@@ -0,0 +1,82 @@
+package com.corejava.multithreading;
+
+import java.util.concurrent.*;
+
+
+
+public class MyThreadPoolExecutor {
+
+    public static void main(String args[]) throws InterruptedException{
+        //RejectedExecutionHandler implementation
+        RejectedExecutionHandlerImpl rejectionHandler = new RejectedExecutionHandlerImpl();
+        //Get the ThreadFactory implementation to use
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        //creating the ThreadPoolExecutor
+        ThreadPoolExecutor executorPool = new ThreadPoolExecutor(2, 4, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2),
+                                                                                                            threadFactory, rejectionHandler);
+        //start the monitoring thread
+        MyMonitorThread monitor = new MyMonitorThread(executorPool, 3);
+        Thread monitorThread = new Thread(monitor);
+        monitorThread.start();
+        //submit work to the thread pool
+        for(int i=0; i<10; i++){
+            executorPool.execute(new WorkerThread("cmd"+i));
+        }
+
+        Thread.sleep(30000);
+        //shut down the pool
+        executorPool.shutdown();
+        //shut down the monitor thread
+        Thread.sleep(5000);
+        monitor.shutdown();
+
+    }
+}
+
+class MyMonitorThread implements Runnable{
+
+    private ThreadPoolExecutor executor;
+    private int seconds;
+    private boolean run=true;
+
+    public MyMonitorThread(ThreadPoolExecutor executor, int delay)
+    {
+        this.executor = executor;
+        this.seconds=delay;
+    }
+    public void shutdown(){
+        this.run=false;
+    }
+    @Override
+    public void run()
+    {
+        while(run){
+            System.out.println(
+                    String.format("[monitor] [%d/%d] Active: %d, Completed: %d, Task: %d, isShutdown: %s, isTerminated: %s",
+                            this.executor.getPoolSize(),
+                            this.executor.getCorePoolSize(),
+                            this.executor.getActiveCount(),
+                            this.executor.getCompletedTaskCount(),
+                            this.executor.getTaskCount(),
+                            this.executor.isShutdown(),
+                            this.executor.isTerminated()));
+            try {
+                Thread.sleep(seconds*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+
+class RejectedExecutionHandlerImpl implements RejectedExecutionHandler{
+
+    @Override
+    public void rejectedExecution(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+        System.out.println(runnable.toString() + " is rejected");
+    }
+}
+
+
